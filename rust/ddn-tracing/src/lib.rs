510460f6e93ff882d7493a2df893b389ee2aca37
Samir Talwar <samir.talwar@hasura.io>
2024-05-02 11:56:19 +0200

    Copy the "tracing-util" from the v3 engine into "ddn-tracing".

--- rust/ddn-tracing/src/lib.rs
+++ rust/ddn-tracing/src/lib.rs
@@ -1,14 +1,24 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod http;
+mod request;
+mod setup;
+mod traceable;
+mod tracer;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// Avoid conflicts with `http` crate
+pub use crate::http::TraceableHttpResponse;
+pub use request::get_trace_headers;
+pub use setup::{shutdown_tracer, start_tracer};
+pub use traceable::{ErrorVisibility, Traceable, TraceableError};
+pub use tracer::{
+    add_event_on_active_span, get_trace_context, global_tracer, set_attribute_on_active_span,
+    set_status_on_current_span, AttributeVisibility, SpanVisibility,
+};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+// re-export things from OpenTelemetry to avoid library users importing their own version and
+// risking mismatches and multiple globals
+pub use opentelemetry::propagation::text_map_propagator::TextMapPropagator;
+pub use opentelemetry::trace::get_active_span;
+pub use opentelemetry::trace::Status;
+pub use opentelemetry::Context;
+pub use opentelemetry_contrib::trace::propagator::trace_context_response::TraceContextResponsePropagator;
+pub use opentelemetry_http::HeaderInjector;
